a8ff847dde97065c8fc7465de0ee93d5ad140568
WalterHorst <whorst42@gmail.com>
2023-11-10 18:53:21 -0300

    Clases listas

--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class OrdenCompra {
+import java.util.Date;
+
+public class OrdenCompra {
+    private static int contadorIds = 0;
+
+    private Integer id;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private Producto[] productos;
+
+    public OrdenCompra(String descripcion) {
+        this.id = ++contadorIds;
+        this.descripcion = descripcion;
+        this.productos = new Producto[4];
+    }
+
 }
---
+++ src/Producto.java
@@ -0,0 +1,13 @@
+public class Producto {
+
+    private String fabricante;
+    private String nombre;
+    private int precio;
+
+    public Producto(String fabricante, String nombre, int precio) {
+        this.fabricante = fabricante;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+}
