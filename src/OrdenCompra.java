ebec36b971d414c8dd5fb60f4f1c2ca37ee5140c
WalterHorst <whorst42@gmail.com>
2023-11-10 19:18:47 -0300

    Finalizada

--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class OrdenCompra {
     private static int contadorIds = 0;
+    private int indiceProductos;
 
     private Integer id;
     private String descripcion;
@@ -15,4 +16,65 @@ public class OrdenCompra {
         this.productos = new Producto[4];
     }
 
+    public static int getContadorIds() {
+        return contadorIds;
+    }
+
+    public static void setContadorIds(int contadorIds) {
+        OrdenCompra.contadorIds = contadorIds;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto[] getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Producto[] productos) {
+        this.productos = productos;
+    }
+
+    public void addProducto(Producto producto) {
+        if (indiceProductos < this.productos.length) {
+            this.productos[indiceProductos++] = producto;
+        }
+    }
+
+    public int getGranTotal(){
+        int total = 0;
+        for(Producto p: productos){
+            total += p.getPrecio();
+        }
+        return total;
+    }
 }
--- src/Producto.java
+++ src/Producto.java
@@ -10,4 +10,27 @@ public class Producto {
         this.precio = precio;
     }
 
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
 }
